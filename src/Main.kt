09d53d192ceb4fa9256a0a3ed4cbb61205941e55
na21hid <naihdasadioun>
2019-08-06 17:26:47 +0430

    Excel file transaction

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,6 @@ import com.example.fileService.fileReading
 import com.example.fileService.readFileExcel
 
 fun main(args: Array<String>) {
-    fileReading("D:/transaction.csv")
+   // fileReading("D:/transaction.csv")
     readFileExcel("D:/Nahid/transactionAnalyser/transaction_master.xlsx")
 }
--- src/com/example/fileService/ReadExcelFile.kt
+++ src/com/example/fileService/ReadExcelFile.kt
@@ -1,7 +1,12 @@
 package com.example.fileService
 
+import com.example.entities.Transaction
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import java.io.FileInputStream
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.Arrays.stream
 
 /*fun main(args: Array<String>) {
     val filepath = "D:/Nahid/transactionAnalyser/transaction_master.xlsx"
@@ -17,9 +22,22 @@ fun readFileExcel(fileName: String) {
     var xlWb = WorkbookFactory.create(inputStream)
     val xlWs = xlWb.getSheetAt(0)
     val xlRows = xlWs.rowIterator()
-    xlRows.forEach { row ->
-        (row.toList())
-        val id = row.getCell(0)
+    xlRows.forEach { eachRow ->
+        (eachRow.toList())
+
+        val trans = Transaction( transactionId = eachRow.getCell(0).toString(),
+            dateAndTime = LocalDateTime.parse(eachRow.getCell(1).toString(),
+                DateTimeFormatter.ofPattern("dd/mm/yyyy HH:mm:ss")),
+            amount = eachRow.getCell(2).toString().toDouble(),
+            merchantName = eachRow.getCell(3).toString(),
+            transactionType = enumValueOf(eachRow.getCell(4).toString()),
+            relatedTransaction = eachRow.getCell(5).toString())
+
+ /*       trans.transactionId = eachRow.getCell(0).toString()
+        println(eachRow.getCell(0))*/
+
+//        trans.dateAndTime = eachRow.getCell(1)
+//        val date = LocalDate.parse( DateTimeFormatter.ISO_DATE)
 
         //row.forEachIndexed({i, e -> println("words[$i] = $e")})
 
